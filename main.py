9795e901038dfc6695b6d88131d2f24a09989218
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2024-05-03 11:40:24 +0530

    fwemfw

--- main.py
+++ main.py
@@ -2,7 +2,35 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
+    page.title="cord"
+    page.bgcolor="black"
+    page.navigation_bar = ft.CupertinoNavigationBar(
+        bgcolor=ft.colors.TRANSPARENT,
+        inactive_color=ft.colors.GREY,
+        active_color="#d3d3d3",
+        on_change=lambda e: print("Selected tab:", e.control.selected_index),
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.HOME, label="Home"),
+            ft.NavigationDestination(icon=ft.icons.PERSON, label="Profile")
+        ]
+    )
+
+    title=ft.SafeArea(ft.Text("cord", size=20, color="#d3d3d3"))
+
+    serverlist=[]
+
+    for i in range(0,30):
+        serverlist.append(ft.Text('i'))
+
+    servers=ft.ListView(serverlist)
+
+    servercont=ft.Column([servers], 
+                         scroll=ft.ScrollMode.HIDDEN, 
+                         expand=True, 
+                         width=50
+                         )
+
+    page.add(title, servercont)
 
 ft.app(main)
